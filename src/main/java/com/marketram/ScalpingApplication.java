131061b5cbbfe9c5c79a02f175f82876b758033a
Eugene Markine <emarkine@gmail.com>
2017-06-25 23:25:29 +0200

    Init with IB API

---
+++ src/main/java/com/marketram/ScalpingApplication.java
@@ -0,0 +1,12 @@
+package com.marketram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalpingApplication.class, args);
+	}
+}
