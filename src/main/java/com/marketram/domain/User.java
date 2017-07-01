50e2df79b09b5d6f783e51b79c134b0f63915fc0
Eugene Markine <emarkine@gmail.com>
2017-07-01 19:37:05 +0200

    2017-07-01.19:37

--- src/main/java/com/marketram/domain/User.java
+++ src/main/java/com/marketram/domain/User.java
@@ -9,7 +9,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String name;
-    private String surname;
+    private String username;
     private String email;
     private String password;
 
@@ -18,7 +18,6 @@ public class User {
 
     public User(String name, String surname, String email, String password) {
         this.name = name;
-        this.surname = surname;
         this.email = email;
         this.password = password;
     }
@@ -39,20 +38,12 @@ public class User {
         this.name = name;
     }
 
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
     public String getUsername() {
-        return email;
+        return username;
     }
 
-    public void setUsername(String email) {
-        this.email = email;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
@@ -61,5 +52,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
